05b30ec8ed8b9e68e8233b67ef00198429ad9a18
Nicolas Martinez <nmartinez@corpit-consulting.com>
2019-05-11 17:51:38 -0300

    New Module for Bacth Queue

---
+++ variables.tf
@@ -0,0 +1,25 @@
+##Variables Batch Job Queue
+
+variable "name" {
+  type        = "string"
+  description = "Specifies the name of the job queue"
+  default     = ""
+}
+
+variable "compute_environments" {
+  type        = "list"
+  description = "Specifies the set of compute environments mapped to a job queue and their order. The position of the compute environments in the list will dictate the order. You can associate up 3 compute environments with a job queue"
+  default     = [] 
+}
+
+variable "priority" {
+  type        = "string"
+  description = "The priority of the job queue. Job queues with a higher priority are evaluated first when associated with the same compute environment"
+  default     = ""
+}
+
+variable "state" {
+  type        = "string"
+  description = "The state of the job queue. Must be one of ENABLED or DISABLED"
+  default     = "ENABLED"
+}
