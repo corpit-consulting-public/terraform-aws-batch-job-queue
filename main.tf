d35cd9433c8ac3b241310c7e2fbc70b2124498c5
Nicolas Martinez <nmartinez@corpit-consulting.com>
2020-09-16 18:55:01 -0300

    upgrade module to terraform 0.12

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_batch_job_queue" "batch_queue" {
-  name                 = "${var.name}"
-  state                = "${var.state}"
-  priority             = "${var.priority}"
-  compute_environments = ["${var.compute_environments}"]
+  name                 = var.name
+  state                = var.state
+  priority             = var.priority
+  compute_environments = var.compute_environments
 }
+
--- variables.tf
+++ variables.tf
@@ -1,21 +1,22 @@
 ##Variables Batch Job Queue
 
 variable "name" {
-  type        = "string"
+  type        = string
   description = "Specifies the name of the job queue"
 }
 
 variable "compute_environments" {
-  type        = "list"
+  type        = list(string)
   description = "Specifies the set of compute environments mapped to a job queue and their order. The position of the compute environments in the list will dictate the order. You can associate up 3 compute environments with a job queue"
 }
 
 variable "priority" {
-  type        = "string"
+  type        = string
   description = "The priority of the job queue. Job queues with a higher priority are evaluated first when associated with the same compute environment"
 }
 
 variable "state" {
-  type        = "string"
+  type        = string
   description = "The state of the job queue. Must be one of ENABLED or DISABLED"
 }
+
